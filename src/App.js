289555acbd9d6dde728a26038524ab0dba361b7f
mbennettlowe <mbennettlowe@gmail.com>
2020-03-08 00:55:22 -0500

    add Course component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Course from './components/Course';
 
+// Creating object function that embeds arrays and additional embedded objects
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      < Course />
     </div>
+    
   );
 }
-
+  
 export default App;
---
+++ src/components/Course.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+// Functional Component consisting of Objects with embedded Arrays - Multidemensional Object
+const Course = () => {
+      const name = "Half Stack application development"
+      const parts = [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2  
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3
+        }
+        ]
+
+      const partItems = parts.map((part) => 
+        <li>{part.name}</li>)
+        
+
+      return (
+        <React.Fragment>
+          <h1>{name}</h1>
+          <ul>{partItems}</ul>
+        </React.Fragment>
+       
+      )
+    }
+    
+
+export default Course;
