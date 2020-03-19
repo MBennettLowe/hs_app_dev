d84d2e6ffb17d2e0116e2dd280a645b14188751c
MBennettLowe <mbennettlowe@gmail.com>
2020-03-19 16:25:44 -0400

    added additonal objects

--- src/components/Course.js
+++ src/components/Course.js
@@ -19,9 +19,31 @@ const Course = () => {
           name: "State of a component",
           exercises: 14,
           id: 3
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4
         }
-        ]
-      
+       ]
+       },
+       {
+          name: "Node.js",
+          id: 2,
+          parts: [
+            {
+              name: "Routing",
+              exercises: 3,
+              id: 1
+            },
+            {
+              name: "Middlewares",
+              exercises: 7,
+              id: 2
+            }
+          ]
+        }
+      ]  
         // Created function to iterate through array to retrieve various objects
       const partItems = parts.map((part) => 
         <li>{part.name} {part.exercises}</li>)
